37ad6aa8ad423518bdce64055f4106bb3b93b693
Jessica Tracy <jessicatracy@callrail.com>
2020-02-20 09:43:46 -0500

    get param names

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
+	"regexp"
 	"github.com/pkg/errors"
 )
+
+const (
+	SSM_FORMAT = `{{ssm\s+(\S+)\s?}}`
+	SSM_PATH_FORMAT = `{{ssm-path\s+(\S+)\s?}}`
+)
+
+type ssmParams struct {
+	params []string
+	pathParams []string
+}
 
 func main() {
 	args := os.Args[1:]
@@ -29,11 +42,28 @@ func main() {
 
 	// Get the values files
 	valueFiles := getValueFiles(args)
-	fmt.Println("Value files:")
-	for _, valueFile := range valueFiles {
-		fmt.Println(valueFile)
+	// concatenate the values files
+	mergedValues, err := mergeValueFiles(valueFiles)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
+	// find the lines that match ssm keywords and get params
+	ssmParams, err := getParams(mergedValues)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	fmt.Println("pathParams:")
+	for _, path := range ssmParams.pathParams {
+		fmt.Println(path)
+	}
+	fmt.Println("params:")
+	for _, param := range ssmParams.params {
+		fmt.Println(param)
+	}
 
 }
 
@@ -47,6 +77,62 @@ func getValueFiles(args []string) []string {
 		}
 	}
 	return valueFiles
+}
+
+func mergeValueFiles(valueFiles []string) ([]string, error) {
+	mergedValues := []string{}
+	for _, valueFile := range valueFiles {
+		lines, err := readLines(valueFile)
+		if err != nil {
+			return nil, errors.Wrapf(err, "error reading value file %s", valueFile)
+		}
+		mergedValues = append(mergedValues, lines...)
+	}
+	return mergedValues, nil
+}
+
+func readLines(valueFile string) ([]string, error) {
+	lines := []string{}
+	file, err := os.Open(valueFile)
+	defer file.Close()
+	if err != nil {
+		return nil, err
+	}
+	reader := bufio.NewReader(file)
+	for {
+		line, err := reader.ReadString('\n') // TODO don't add blank lines
+		if err != nil {
+			if err == io.EOF {
+				break
+			} else {
+				return nil, err
+			}
+		}
+		lines = append(lines, line)
+	}
+	return lines, nil
+}
+
+func getParams(values []string) (*ssmParams, error) {
+	reSSM := regexp.MustCompile(SSM_FORMAT)
+	reSSMPath := regexp.MustCompile(SSM_PATH_FORMAT)
+	ssmParams := ssmParams{params: []string{}, pathParams: []string{}}
+	for _, line := range values {
+		if reSSMPath.MatchString(line) {
+			paramSubmatch := reSSMPath.FindStringSubmatch(line)
+			if len(paramSubmatch) < 2 {
+				return nil, errors.New(fmt.Sprintf("format error in line %s", line))
+			}
+			ssmParams.pathParams = append(ssmParams.pathParams, paramSubmatch[1])
+		} else if reSSM.MatchString(line) {
+			paramSubmatch := reSSM.FindStringSubmatch(line)
+			if len(paramSubmatch) < 2 {
+				return nil, errors.New(fmt.Sprintf("format error in line %s", line))
+			}
+			ssmParams.params = append(ssmParams.params, paramSubmatch[1])
+		}
+	}
+	return &ssmParams, nil
 }
 
 func checkForInstall(args []string, index int) bool {
