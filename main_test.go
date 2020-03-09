0a7424ce0980d821aad948adb47fef82fa166c09
Jessica Tracy <jessicatracy@callrail.com>
2020-03-09 17:12:01 -0400

    tests

--- main_test.go
+++ main_test.go
@@ -4,6 +4,56 @@ import (
 	"testing"
 )
 
+func TestPullValueFiles(t *testing.T) {
+	testCases := []struct {
+		name                   string
+		args                   []string
+		expectedValueFiles     []string
+		expectedNewArgs        []string
+	}{
+		{
+			name: "one values file with -f",
+			args: []string{"-n", "test", "upgrade", "release", "my/chart", "-f", "override-values.yaml", "--dry-run"},
+			expectedValueFiles: []string{"override-values.yaml"},
+			expectedNewArgs: []string{"-n", "test", "upgrade", "release", "my/chart", "--dry-run"},
+		},
+		{
+			name: "lots of values files",
+			args: []string{"install", "release", "my/chart", "--values", "values-1.yaml", "-f", "values-2.yaml", "--dry-run", "--values", "values-3.yaml"},
+			expectedValueFiles: []string{"values-1.yaml", "values-2.yaml", "values-3.yaml"},
+			expectedNewArgs: []string{"install", "release", "my/chart", "--dry-run"},
+		},
+		{
+			name: "no value files",
+			args: []string{"install", "-n", "namespace", "release", "my/chart"},
+			expectedValueFiles: []string{},
+			expectedNewArgs: []string{"install", "-n", "namespace", "release", "my/chart"},
+		},
+	}
+	for _, tc := range testCases {
+		tc := tc
+		t.Run(tc.name, func(subT *testing.T) {
+			resultValueFiles, resultNewArgs := pullValueFiles(tc.args)
+			if len(resultValueFiles) != len(tc.expectedValueFiles) {
+				subT.Errorf("expected %d value files, got %d\n", len(tc.expectedValueFiles), len(resultValueFiles))
+			}
+			for i, f := range resultValueFiles {
+				if f != tc.expectedValueFiles[i] {
+					subT.Errorf("expected value file %s, got %s\n", tc.expectedValueFiles[i], f)
+				}
+			}
+			if len(resultNewArgs) != len(tc.expectedNewArgs) {
+				subT.Errorf("expected %d new args, got %d\n", len(tc.expectedNewArgs), len(resultNewArgs))
+			}
+			for i, a := range resultNewArgs {
+				if a != tc.expectedNewArgs[i] {
+					subT.Errorf("expected new arg %s, got %s\n", tc.expectedNewArgs[i], a)
+				}
+			}
+		})
+	}
+}
+
 func TestConstructReplacementLine(t *testing.T) {
 	testCases := []struct {
 		name          string
