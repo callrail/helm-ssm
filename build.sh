0495cfb1a989a2feb55c48fdfee65e10f60eb6ce
James Denton <james@callrail.com>
2021-04-16 17:40:39 -0400

    Add a Darwin-arm64 build

--- build.sh
+++ build.sh
@@ -4,3 +4,4 @@ go get -d ./...
 go test .
 env GOOS=linux GOARCH=amd64 go build -o helm-ssm-Linux-x86_64 main.go
 env GOOS=darwin GOARCH=amd64 go build -o helm-ssm-Darwin-x86_64 main.go
+env GOOS=darwin GOARCH=arm64 go build -o helm-ssm-Darwin-arm64 main.go
